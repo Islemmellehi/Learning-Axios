77d0076f70ce17d5c3d74d8a54accd304be1da01
islem mellehi <islemmellehi222@gmail.com>
2021-08-25 18:26:43 +0100

    modal style change

--- src/components/Get.js
+++ src/components/Get.js
@@ -3,43 +3,46 @@ import { useState } from "react";
 import Modal from "react-modal";
 
 function Get(props) {
-  const [x,setX]= useState(null);
+  const [x, setX] = useState(null);
   const [modalisOpen, setModalIsOpen] = useState(false);
-  const[loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  
   function closeModelHandler() {
     setModalIsOpen(false);
   }
 
-  
   function get_shit() {
-    setLoading(true) 
+    setLoading(true);
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
       .then((res) => Showshit(res));
     function Showshit(res) {
       //variable containing output
-      setX(JSON.stringify(res.data, null, 2))
+      setX(JSON.stringify(res.data, null, 2));
       setModalIsOpen(true);
-      setLoading(false)
+      setLoading(false);
     }
-    
-    
-
   }
 
   return (
     <div>
       <div>
-        {loading ? <p>Loading..</p> :<button onClick={get_shit} id="get">Get Data</button>}
+        {loading ? (
+          <p>Loading..</p>
+        ) : (
+          <button onClick={get_shit} id="get">
+            Get Data
+          </button>
+        )}
         <p id="response"></p>
       </div>
       <div id="dumbfuckmodal">
-        <Modal id="modal" isOpen={modalisOpen} >
+        <Modal id="modal" isOpen={modalisOpen}>
           <h1>Data</h1>
           <p id="modalresponse">{x} </p>
-          <button id="close" onClick={closeModelHandler}>Close </button>
+          <button id="close" onClick={closeModelHandler}>
+            Close{" "}
+          </button>
         </Modal>
       </div>
     </div>
